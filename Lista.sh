72186ab56f1a392fb9b0515496749520a71082c2
Daniel Perez <daniel.perezmorera@ucr.ac.cr>
2022-12-04 02:08:15 -0600

    Compiladores

---
+++ Lista.sh
@@ -0,0 +1,2 @@
+g++ -std=c++20 src/AlgortimosGrafo.cpp src/Menu.cpp src/ListaDeAdyacencia/Grafo.cpp -o ejecutable
+./ejecutable
---
+++ Matriz.sh
@@ -0,0 +1,2 @@
+g++ -std=c++20 src/AlgortimosGrafo.cpp src/Menu.cpp src/MatrizAdyacencia/Grafo.cpp -o ejecutable
+./ejecutable
