e2f864b969d0f9bffaa8984fde91701ab9d7bd16
Sebastian Sandi <sebastian.sanchezsandi@ucr.ac.cr>
2022-12-05 00:01:25 -0600

    BERA

--- src/matrizAdyacencia/main.cpp
+++ src/matrizAdyacencia/main.cpp
@@ -20,7 +20,8 @@ int calculoCotaInferior(Grafo& grafo, std::vector<std::vector<int>>& matriz,
 
 void CircuitoHamiltonBERA(const int n, Vertice** solActual, Vertice** solMejor
   , Diccionario<Vertice*>& DDvisitados, int& pesoCaminoActual
-  , unsigned int& pesoMejorCamino, int i, Grafo& grafo);
+  , unsigned int& pesoMejorCamino, Vertice* v, Grafo& grafo
+  , std::vector<std::vector<int>>& matriz, R11<Vertice*, int>& r11);
 
 
 Vertice* buscarVertice(Grafo& grafo, char etiqueta) {
@@ -64,19 +65,19 @@ int main() {
     vertice = grafo.SiguienteVertice(vertice);
   }
 
-  AgregarAristaLibre('a', 'b', 15, grafo);
-  AgregarAristaLibre('a', 'c', 1, grafo);
-  AgregarAristaLibre('a', 'd', 7, grafo);
-  AgregarAristaLibre('a', 'e', 10, grafo);
+  AgregarAristaLibre('a', 'b', 7, grafo);
+  AgregarAristaLibre('a', 'c', 6, grafo);
+  AgregarAristaLibre('a', 'd', 4, grafo);
+  AgregarAristaLibre('a', 'e', 5, grafo);
 
-  AgregarAristaLibre('b', 'c', 3, grafo);
-  AgregarAristaLibre('b', 'd', 12, grafo);
+  AgregarAristaLibre('b', 'c', 9, grafo);
+  AgregarAristaLibre('b', 'd', 3, grafo);
   AgregarAristaLibre('b', 'e', 8, grafo);
   
-  AgregarAristaLibre('c', 'd', 4, grafo);
-  AgregarAristaLibre('c', 'e', 9, grafo);
+  AgregarAristaLibre('c', 'd', 2, grafo);
+  AgregarAristaLibre('c', 'e', 11, grafo);
 
-  AgregarAristaLibre('d', 'e', 5, grafo);
+  AgregarAristaLibre('d', 'e', 10, grafo);
   
   Vertice* vAdy = grafo.PrimerVerticeAdyacente(buscarVertice(grafo, 'a'));
   std::cout << grafo.Etiqueta(vAdy) << std::endl;
@@ -117,18 +118,24 @@ void CircuitoHamilton(Grafo& grafo, R11<Vertice*, int>& r11) {
 
   std::vector<std::vector<int>> matriz;
   CreacionMatriz(grafo, r11, matriz, n);
-  std::cout << calculoCotaInferior(grafo, matriz, r11) << std::endl;
 
-  // CircuitoHamiltonBERA(n, solActual, solMejor, DDvisitados, pesoCaminoActual
-  //   , pesoMejorCamino, 2, grafo);
+  CircuitoHamiltonBERA(n, solActual, solMejor, DDvisitados, pesoCaminoActual
+    , pesoMejorCamino, grafo.PrimerVertice(), grafo, matriz, r11);
+  
 
-  /*
+  std::cout << "Matriz final" << std::endl;
+  for (int i = 0; i < 5; ++i) {
+    for (int j = 0; j < 5; ++j) {
+      std::cout << matriz[i][j] << "  ";
+    }
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
   for (int i = 0; i < n; ++i) {
     std::cout << grafo.Etiqueta(solMejor[i]) << "->";
   }
   std::cout << grafo.Etiqueta(solMejor[0]) << std::endl;
   std::cout << "Peso: " << pesoMejorCamino << std::endl;
-  */
 }
 
 void CreacionMatriz(Grafo& grafo, R11<Vertice*, int>& r11
@@ -154,6 +161,224 @@ void CreacionMatriz(Grafo& grafo, R11<Vertice*, int>& r11
       vAdy = grafo.SiguienteVerticeAdyacente(v, vAdy);
     }
     v = grafo.SiguienteVertice(v);
+  }
+}
+
+bool revisarFila(std::vector<std::vector<int>>& matriz, int i) {
+  bool completa = false;
+  int contador = 0;
+  int contadorTotal = 0;
+  for (int j = 0; j < matriz[i].size(); ++j) {
+    if (matriz[i][j] == -1) {
+      ++contador;
+      ++contadorTotal;
+    } else if (matriz[i][j] == 1) {
+      ++contadorTotal;
+    }
+  }
+
+  if (contador == 2) {
+    completa = true;
+    for (int j = 0; j < matriz[i].size(); ++j) {
+      if (matriz[i][j] != -1) {
+        matriz[i][j] = 0;
+        matriz[j][i] = 0;
+      }
+    }
+  } else if (contadorTotal == 2) {
+    completa = true;
+    for (int j = 0; j < matriz[i].size(); ++j) {
+      if (matriz[i][j] == 1) {
+        matriz[i][j] = -1;
+        matriz[j][i] = -1;
+      }
+    }
+  }
+  return completa;
+}
+
+int revisarMatriz(std::vector<std::vector<int>>& matriz, int n) {
+  int cantidadAristasFijas = 0;
+  for (int i = 0; i < n; ++i) {
+    int contadorAristas = 0;
+    int contadorTotal = 0;
+    for (int j = 0; j < n; ++j) {
+      if (matriz[i][j] == 1) {
+        ++contadorAristas;
+        ++contadorTotal;
+      } else if(matriz[i][j] != 0) {
+        ++contadorTotal;
+      }
+    }
+
+    if (contadorAristas == 2 && contadorTotal == contadorAristas) {
+      for (int j = 0; j < matriz[i].size(); ++j) {
+        if (matriz[i][j] == 1) {
+          matriz[i][j] = -1;
+          matriz[j][i] = -1;
+        }
+      }
+    }
+  }
+  for (int vuelta = 1; vuelta <= 2; ++vuelta) {
+    for (int i = 0; i < n; ++i) {
+      if (revisarFila(matriz, i) && vuelta == 2) {
+        cantidadAristasFijas += 2;
+      }
+    }
+  }
+
+  return cantidadAristasFijas;
+}
+
+int calcularPesoActual(std::vector<std::vector<int>>& matriz, int n
+  , R11<Vertice*, int>& r11, Grafo& grafo) {
+  int peso = 0;
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+      if (matriz[i][j] == -1) {
+        peso += grafo.Peso(r11.PreImg(i), r11.PreImg(j));
+      }
+    }
+  }
+  peso = peso / 2;
+  return peso;
+}
+
+void CopiarSolucionBERA(int n, std::vector<std::vector<int>>& matriz
+  , Vertice** solMejor, R11<Vertice*, int>& r11) {
+  Diccionario<Vertice*> DDvisitados;
+
+  solMejor[0] = r11.PreImg(0);
+  DDvisitados.Agregar(solMejor[0]);
+  int contador = 1;
+  int siguienteVertice = 0;
+  while (contador < n) {
+    for (int j = 0; j < n; ++j) {
+      if (matriz[siguienteVertice][j] == -1 &&
+        DDvisitados.Pertenece(r11.PreImg(j)) == DDvisitados.NotFound()) {
+        DDvisitados.Agregar(r11.PreImg(j));
+        solMejor[contador] = r11.PreImg(j);
+        siguienteVertice = j;
+        j = n;
+        ++contador;
+      }
+    }
+  }
+}
+
+void CopiarMatrices(int n, std::vector<std::vector<int>>& matriz
+  , std::vector<std::vector<int>>& matrizCopia) {
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+      matrizCopia[i][j] = matriz[i][j];
+    }
+  }
+}
+
+void CircuitoHamiltonBERA(const int n, Vertice** solActual, Vertice** solMejor
+  , Diccionario<Vertice*>& DDvisitados, int& pesoCaminoActual
+  , unsigned int& pesoMejorCamino, Vertice* v, Grafo& grafo
+  , std::vector<std::vector<int>>& matriz, R11<Vertice*, int>& r11) {
+  bool sePodo = false;
+
+  DDvisitados.Agregar(v);
+  
+  int cotaInferiorLadoIzq = 0;
+  int cotaInferiorLadoDer = 0;
+
+  Vertice* vAdy = grafo.PrimerVerticeAdyacente(v);
+  while (vAdy != verticeNulo) {
+    int ii = r11.Img(v);
+    int j = r11.Img(vAdy);
+    if (matriz[ii][j] &&
+      DDvisitados.Pertenece(vAdy) == DDvisitados.NotFound()) {
+      matriz[ii][j] = -1;
+      matriz[j][ii] = -1;
+      cotaInferiorLadoIzq = calculoCotaInferior(grafo, matriz, r11);
+      matriz[ii][j] = 0;
+      matriz[j][ii] = 0;
+      cotaInferiorLadoDer = calculoCotaInferior(grafo, matriz, r11);
+    
+      if (cotaInferiorLadoIzq < cotaInferiorLadoDer &&
+        cotaInferiorLadoIzq < pesoMejorCamino) {
+        std::vector<std::vector<int>> matrizCopia;
+        CreacionMatriz(grafo, r11, matrizCopia, n);
+        CopiarMatrices(n, matriz, matrizCopia);
+        matriz[ii][j] = -1;
+        matriz[j][ii] = -1;
+
+        // Antes de cambiar la matriz guardar una copia
+
+        if (revisarFila(matriz, ii)) {
+          pesoCaminoActual += grafo.Peso(v, vAdy);
+          if (revisarMatriz(matriz, n) == (n * 2)) {
+            pesoCaminoActual = calcularPesoActual(matriz, n, r11, grafo);
+            if (pesoMejorCamino > pesoCaminoActual +
+              grafo.Peso(vAdy, solActual[0])) {
+              pesoMejorCamino = pesoCaminoActual;
+              sePodo = true;
+              CopiarSolucionBERA(n, matriz, solMejor, r11);
+            }
+          } else {
+            CircuitoHamiltonBERA(n, solActual, solMejor, DDvisitados
+              , pesoCaminoActual , pesoMejorCamino
+              , grafo.SiguienteVertice(v), grafo, matriz, r11);
+            sePodo = true;
+            if (pesoMejorCamino > cotaInferiorLadoDer) {
+               sePodo = false;
+              // Matriz muy modificada, hay que revertir los cambios !!
+              CopiarMatrices(n, matrizCopia, matriz);
+              matriz[ii][j] = 0;
+              matriz[j][ii] = 0;
+              revisarMatriz(matriz, n);
+            }
+          }
+        } else {
+          revisarMatriz(matriz, n);
+          pesoCaminoActual += grafo.Peso(v, vAdy);
+        }
+      } else if (cotaInferiorLadoDer < pesoMejorCamino) {
+        std::vector<std::vector<int>> matrizCopia;
+        CreacionMatriz(grafo, r11, matrizCopia, n);
+        CopiarMatrices(n, matriz, matrizCopia);
+        matriz[ii][j] = 0;
+        matriz[j][ii] = 0;
+        if (revisarFila(matriz, ii)) {
+          pesoCaminoActual += grafo.Peso(v, vAdy);
+          if (revisarMatriz(matriz, n) == (n * 2)) {
+            pesoCaminoActual = calcularPesoActual(matriz, n, r11, grafo);
+            if (pesoMejorCamino > pesoCaminoActual +
+              grafo.Peso(vAdy, solActual[0])) {
+              pesoMejorCamino = pesoCaminoActual;
+              sePodo = true;
+              CopiarSolucionBERA(n, matriz, solMejor, r11);
+            }
+          } else {
+            CircuitoHamiltonBERA(n, solActual, solMejor, DDvisitados
+              , pesoCaminoActual , pesoMejorCamino,
+              grafo.SiguienteVertice(v), grafo, matriz, r11);
+              sePodo = true;
+            if (pesoMejorCamino > cotaInferiorLadoIzq) {
+              sePodo = false;
+              CopiarMatrices(n, matrizCopia, matriz);
+              matriz[ii][j] = -1;
+              matriz[j][ii] = -1;
+              revisarMatriz(matriz, n);
+            }
+          }
+        } else {
+          revisarMatriz(matriz, n);
+          pesoCaminoActual += grafo.Peso(v, vAdy);
+        }
+      }
+    }
+
+    if (!sePodo) {
+      vAdy = grafo.SiguienteVerticeAdyacente(v, vAdy);
+    } else {
+      vAdy = verticeNulo;
+    }
   }
 }
 
@@ -187,36 +412,33 @@ void CircuitoHamiltonBEP(const int n, Vertice** solActual, Vertice** solMejor
 }
 */
 
-/*
-void CircuitoHamiltonBERA(const int n, Vertice** solActual, Vertice** solMejor
-  , Diccionario<Vertice*>& DDvisitados, int& pesoCaminoActual
-  , unsigned int& pesoMejorCamino, int i, Grafo& grafo) {
-    std::cout << "Calcular cota inferior" << std::endl;
-    int cotaInferior = primeraCotaInferior(grafo);
-
-    std::cout << cotaInferior << std::endl;
-}
-*/
-
 int calculoCotaInferior(Grafo& grafo, std::vector<std::vector<int>>& matriz,
   R11<Vertice*, int>& r11) {
+ 
   bool unaAristFija = false;
   bool dosAristasFijas = false;
   int aristaFijaUno = 0;
   int aristaFijaDos = 0;
   int cotaInferior = 0;
-  unsigned int menor = -1;
-  unsigned int segundoMenor = -1;
+  int menor = -1;
+  int segundoMenor = -1;
+  int peso = 0;
   Vertice* v = grafo.PrimerVertice();
   while (v != verticeNulo) {
     Vertice* vAdy = grafo.PrimerVerticeAdyacente(v);
     while (vAdy != verticeNulo) {
       if (matriz[r11.Img(v)][r11.Img(vAdy)]) {
         if (matriz[r11.Img(v)][r11.Img(vAdy)] > 0) {
-          if (grafo.Peso(v, vAdy) < menor) {
-            menor = grafo.Peso(v, vAdy);
-          } else if (grafo.Peso(v, vAdy) < segundoMenor) {
-            segundoMenor = grafo.Peso(v, vAdy);
+          peso = grafo.Peso(v, vAdy);
+          if (menor == -1 || peso < menor) {
+            if (menor != -1) {
+              segundoMenor = menor;
+              menor = peso;
+            } else {
+              menor = peso;
+            }
+          } else if (segundoMenor == -1 || peso < segundoMenor) {
+            segundoMenor = peso;
           }
         } else {
           if (!unaAristFija) {
@@ -224,13 +446,15 @@ int calculoCotaInferior(Grafo& grafo, std::vector<std::vector<int>>& matriz,
             unaAristFija = true;
           } else {
             aristaFijaDos = grafo.Peso(v, vAdy);
+            unaAristFija = false;
             dosAristasFijas = true;
           }
         }
       }
       vAdy = grafo.SiguienteVerticeAdyacente(v, vAdy);
     }
-    if (!unaAristFija) {
+
+    if (!unaAristFija && !dosAristasFijas) {
       cotaInferior += (menor + segundoMenor);
     } else {
       if (unaAristFija) {
